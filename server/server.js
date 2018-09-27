5cc5431caaee72c484f1fc60d8509d5545fda258
Harkame <louisr.daviaud@gmail.com>
2018-09-27 16:51:43 +0200

    dev of search by name

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const app=express();
-// const bodyParser = require('body-parser');
-const cors = require('cors');
-const ObjectID = require('mongodb').ObjectID;
-app.use(cors());
+const app = express();
 app.use(express.json());
-// const asyncHandler = require('express-async-handler');
-const async = require('async');
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.json);
-// app.use(express.urlencoded({extended : true}))
 
-//creation client
+const Iconv  = require('iconv').Iconv;
+
+const cors = require('cors');
+app.use(cors());
+
+const request = require('request');
+
+const HTMLParser = require('node-html-parser');
 
 const sendRes = function(res, json){
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -23,7 +21,7 @@ app.all("/*", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  console.log("Requete recue");
+  console.log("New request");
   next();
 });
 
@@ -31,47 +29,23 @@ app.all("/*", function(req, res, next){
 app.get("/search/word/:word",function(req,res){
 
   let word = req.params.word;
-  console.log('word : ' + word);
 
-  sendRes(res, JSON.stringify(word));
+  var iconv = new Iconv('UTF-8','LATIN1');
+  var encodedWord = iconv.convert(word).toString();
+
+  let url = 'http://www.jeuxdemots.org/rezo-xml.php?gotermsubmit=Chercher&gotermrel=' + encodedWord + '&output=onlyxml';
+
+  console.log(url);
+
+  request(url, function (error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+
+    let root = HTMLParser.parse(body);
+
+
+    sendRes(res, JSON.stringify(root));
+  });
 });
-
-// Search by relation
-app.get("/search/relation/:relation",function(req,res){
-
-  var oid = new ObjectID(req.params.id);
-  console.log('id : ' + req.params.id);
-  database.collection("membres").find( {"_id": oid} )
-    .toArray(function(err,documents){
-      console.log("Recherche de membre par id : " + req.params.id);
-      delete documents[0].password;
-      console.log(documents);
-      var json = JSON.stringify(documents);
-      sendRes(res, json);
-    });
-});
-
-// membre par id
-app.get("/membres/id/:id",function(req,res){
-
-  var oid = new ObjectID(req.params.id);
-  console.log('id : ' + req.params.id);
-  database.collection("membres").find( {"_id": oid} )
-  // database.collection("membres").find( {"_id.$oid": req.params.id} )
-    .toArray(function(err,documents){
-      console.log("Recherche de membre par id : " + req.params.id);
-      delete documents[0].password; // ne pas renvoyer le mdp
-      console.log(documents);
-      var json = JSON.stringify(documents);
-      sendRes(res, json);
-    });
-});
-
-
-
-
-
-
-
 
 app.listen(8888);
