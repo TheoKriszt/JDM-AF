f2a816d2214184ac849863b62130dbdb427c5ea8
Anthony <anthony-34400@hotmail.fr>
2018-10-04 19:19:23 +0200

    Add bootstrap in angular (import...), refont of app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {WordsModule} from './words/words.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule} from '@angular/material/card';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 
@@ -13,7 +15,7 @@ import {AppRoutingModule} from './app-routing.module';
   ],
   imports: [
     WordsModule, BrowserModule, BrowserAnimationsModule, RouterModule,
-    AppRoutingModule
+    AppRoutingModule, MatCardModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
