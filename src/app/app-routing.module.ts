dcad6096dcf4e286599dcbde7d6fe667b3888d23
wizehunt <theo@kriszt.fr>
2019-01-18 16:53:14 +0100

    Fix : Route vers words-search ne fonctionnait pas en dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,28 @@
 import {NgModule} from '@angular/core';
 import {WordsModule} from './words/words.module';
 import {RouterModule, Routes} from '@angular/router';
+import {environment} from '../environments/environment';
 
 
-const routes: Routes = [
-  {
-    path: 'words',
-    // loadChildren: () => WordsModule
-    loadChildren: './words/words.module#WordsModule'
-  }
-];
+let routes: Routes;
+
+if (environment.production) {
+  routes = [
+      {
+        path: 'words',
+        // loadChildren: () => WordsModule
+        loadChildren: './words/words.module#WordsModule'
+      }
+    ];
+} else {
+  routes = [
+    {
+      path: 'words',
+      loadChildren: () => WordsModule
+      // loadChildren: './words/words.module#WordsModule'
+    }
+  ];
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,
